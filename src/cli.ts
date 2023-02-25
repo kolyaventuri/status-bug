ddeca77242f084401816233393cf46f8871c4bcf
Kolya Venturi <kolya.venturi@enx3s.com>
2023-02-25 11:37:28 -0700

    Refactor into a CLI and importable version

---
+++ src/cli.ts
@@ -0,0 +1,26 @@
+import process from 'node:process';
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+import {loadConfig} from './config';
+import {LOGO} from './constants';
+import {createClient} from '.';
+
+const argv = yargs(hideBin(process.argv))
+  .options({
+    config: {type: 'string', alias: 'c'},
+  })
+  .parseSync();
+
+const configFile = argv.config ?? argv._[0]?.toString();
+
+if (!configFile) {
+  throw new Error('Configuration file was not provided.');
+}
+
+(async () => {
+  const config = await loadConfig(configFile);
+
+  console.log(LOGO);
+
+  createClient(config);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,9 @@
-import process from 'node:process';
-import yargs from 'yargs';
-import {hideBin} from 'yargs/helpers';
-import Client from './client';
-import {loadConfig} from './config';
-import {LOGO} from './constants';
+import {type Configuration} from './config';
 import SocketClient from './socket-client';
 import {createServer} from './http-server';
+import Client from './client';
 
-const argv = yargs(hideBin(process.argv))
-  .options({
-    config: {type: 'string', alias: 'c'},
-  })
-  .parseSync();
-
-const configFile = argv.config ?? argv._[0]?.toString();
-
-if (!configFile) {
-  throw new Error('Configuration file was not provided.');
-}
-
-(async () => {
-  const config = await loadConfig(configFile);
-
-  console.log(LOGO);
-
+export const createClient = (config: Configuration) => {
   const statusClient = new Client(config);
   const server = createServer();
   const socketClient = new SocketClient(server, config);
@@ -31,4 +11,6 @@ if (!configFile) {
   statusClient.on('serviceStatusChanged', (service: string, status: string) => {
     socketClient.sendStatus(service, status);
   });
-})();
+
+  return statusClient;
+};
